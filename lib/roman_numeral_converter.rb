ceeb58e348c7529f4ee231ec29f4586ca41a647e
Natalie Blackburn & KeMonté Jones <student@epicodus.com>
2014-03-05 10:43:13 -0800

    converts any number up to 3999 to roman numerals

--- lib/roman_numeral_converter.rb
+++ lib/roman_numeral_converter.rb
@@ -1,12 +1,60 @@
 def roman_numeral_converter (input)
   output = []
+  if input >= 1000
+    while input >= 1000
+    output << "M"
+    input -= 1000
+    end
+  end
+  if input >= 900
+    output << "DM"
+    input -= 900
+  end
+  if input >= 500
+    output << "D"
+    input -= 500
+  elsif input >= 400
+    output << "CD"
+    input -= 400
+  end
+  if input >= 100
+    while input >= 100
+      output << "C"
+      input -= 100
+    end
+  end
+  if input >= 90
+    output << "XC"
+    input -= 90
+  end
+  if input >= 50
+    output << "L"
+    input -= 50
+  elsif input >= 40
+    output << "XL"
+    input -=40
+  end
+  if input >= 10
+    while input >= 10
+      output << "X"
+      input -= 10
+    end
+  end
+  if input == 9
+    output << "IX"
+    input -= 9
+  end
+  remainder = input % 5
   if input / 5 >= 1
-    remainder = input % 5
+    output << "V"
+  elsif remainder == 4
+    output << "IV"
   else
-    while input > 0 do
+    while remainder > 0 do
       output << "I"
-      input -= 1
+      remainder -= 1
     end
   end
   output.join
 end
+
--- spec/roman_numeral_converter_spec.rb
+++ spec/roman_numeral_converter_spec.rb
@@ -11,4 +11,34 @@ describe ("roman_numeral_converter") do
   it('converts the value four to the symbol IV') do
     roman_numeral_converter(4).should(eq("IV"))
   end
+  it('converts the value ten to the symbol X') do
+    roman_numeral_converter(10).should(eq("X"))
+  end
+  it('converts the value nine to the symbol IX') do
+    roman_numeral_converter(9).should(eq("IX"))
+  end
+  it('converts the value fifty to the symbol L') do
+    roman_numeral_converter(50).should(eq("L"))
+  end
+  it('converts the value forty-five to the symbol XLV') do
+    roman_numeral_converter(45).should(eq("XLV"))
+  end
+  it('converts the value of one hundred to the symbol C') do
+    roman_numeral_converter(100).should(eq("C"))
+  end
+  it('converts the value ninety-nine to the symbol XCIX') do
+    roman_numeral_converter(99).should(eq("XCIX"))
+  end
+  it('converts the value of five hundred to the symbol D') do
+    roman_numeral_converter(500).should(eq("D"))
+  end
+  it('converts the value of four hundred forty-five to the symbol CDXLV') do
+    roman_numeral_converter(445).should(eq("CDXLV"))
+  end
+  it('converts the value of one thousand to the symbol') do
+    roman_numeral_converter(1000).should(eq("M"))
+  end
+  it('converts the value of three thousand nine hundred ninety-nine to the symbol MMMDMXCIX') do
+    roman_numeral_converter(3999).should(eq("MMMDMXCIX"))
+  end
 end
